329eb8bcb21695cf134e041f7ece26bb16cd1909
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2014-10-03 18:54:01 +0200

    A per-process put/get operation.

---
+++ src/jsg_store.erl
@@ -0,0 +1,41 @@
+-module(jsg_store).
+
+-export([put/2,get/1]).
+
+put(Key,Value) ->
+  ensure_open(),
+  ets:insert(jsg_store,{Key,Value}).
+
+get(Key) ->
+  try ets:lookup(jsg_store,Key) of
+    [{Key,Value}] -> {ok,Value};
+    _ -> false
+  catch _:_ -> false end.
+      
+ensure_open() ->
+  case ets:info(jsg_store) of
+    undefined ->
+      open_db();
+    _ ->
+      ok
+  end.
+
+open_db() ->
+  spawn(fun () ->
+	    ets:new(jsg_store,[named_table,public]),
+	    wait_forever()
+	end),
+  wait_until_stable().
+
+wait_forever() ->
+  receive _ -> wait_forever() end.
+
+wait_until_stable() ->
+  case ets:info(jsg_store) of
+    undefined ->
+      timer:sleep(10),
+      wait_until_stable();
+    _ ->
+      ok
+  end.
+  
